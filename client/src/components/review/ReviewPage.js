e2162e7db9f5be0ebb2fc1112f24576666a8bc39
meezumi <aaryank3000@gmail.com>
2025-08-06 18:15:42 +0530

    failure - tried implimenting @mentions for users and notifications, led to having problems with reviewPage, traced back to the last commit :(

--- client/src/components/review/ReviewPage.js
+++ client/src/components/review/ReviewPage.js
@@ -26,6 +26,7 @@ import {
 import AutoFixHighIcon from '@mui/icons-material/AutoFixHigh'; 
 import { motion } from "framer-motion";
 
+
 const pageVariants = {
   initial: { opacity: 0, scale: 0.9 },
   in: { opacity: 1, scale: 1 },
